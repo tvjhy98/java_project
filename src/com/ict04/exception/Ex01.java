5e9dc2d9d30aab53c4bd762f0eb5f69246e35986
tvjhy98 <tvjhy@naver.com>
2021-04-05 15:06:27 +0900

    "ㅇㅁㅇ"

---
+++ src/com/ict04/exception/Ex01.java
@@ -0,0 +1,10 @@
+package com.ict04.exception;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		// 자바에서 오류가 발생하면 무조건 프로그램을 종료시킨다
+		// 그러나, 가벼운 예외나 예상된 예외를 별도로 처리하는 방법을 사용하면 
+		// 무조건 프로그램이 종료되는것이 아니라 정상 종료를 할수 있도록 하는방법을 
+		// 예외 처리하고 한다 
+	}
+}
